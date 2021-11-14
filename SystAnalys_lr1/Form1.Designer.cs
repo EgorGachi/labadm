4fe9c7f9943df91efe5580bd2fdd71aba833af3f
egorm <egorm@DESKTOP-B7DPUHA>
2021-11-14 20:26:25 +0300

    Добавьте файлы проекта.

---
+++ SystAnalys_lr1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SystAnalys_lr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMatrix = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cycleButton = new System.Windows.Forms.Button();
+            this.chainButton = new System.Windows.Forms.Button();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.deleteALLButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.drawEdgeButton = new System.Windows.Forms.Button();
+            this.drawVertexButton = new System.Windows.Forms.Button();
+            this.sheet = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMatrix
+            // 
+            this.listBoxMatrix.FormattingEnabled = true;
+            this.listBoxMatrix.ItemHeight = 16;
+            this.listBoxMatrix.Location = new System.Drawing.Point(977, 34);
+            this.listBoxMatrix.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxMatrix.Name = "listBoxMatrix";
+            this.listBoxMatrix.Size = new System.Drawing.Size(288, 132);
+            this.listBoxMatrix.TabIndex = 6;
+            this.listBoxMatrix.SelectedIndexChanged += new System.EventHandler(this.listBoxMatrix_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1292, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cycleButton
+            // 
+            this.cycleButton.Image = global::SystAnalys_lr1.Properties.Resources.cycle;
+            this.cycleButton.Location = new System.Drawing.Point(1172, 198);
+            this.cycleButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cycleButton.Name = "cycleButton";
+            this.cycleButton.Size = new System.Drawing.Size(93, 55);
+            this.cycleButton.TabIndex = 11;
+            this.cycleButton.UseVisualStyleBackColor = true;
+            this.cycleButton.Click += new System.EventHandler(this.CycleButton_Click);
+            // 
+            // chainButton
+            // 
+            this.chainButton.Image = global::SystAnalys_lr1.Properties.Resources.chain;
+            this.chainButton.Location = new System.Drawing.Point(977, 198);
+            this.chainButton.Margin = new System.Windows.Forms.Padding(4);
+            this.chainButton.Name = "chainButton";
+            this.chainButton.Size = new System.Drawing.Size(93, 55);
+            this.chainButton.TabIndex = 10;
+            this.chainButton.UseVisualStyleBackColor = true;
+            this.chainButton.Click += new System.EventHandler(this.ChainButton_Click);
+            // 
+            // selectButton
+            // 
+            this.selectButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.selectButton.Location = new System.Drawing.Point(16, 34);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(4);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(80, 55);
+            this.selectButton.TabIndex = 9;
+            this.selectButton.Text = "choose";
+            this.selectButton.UseVisualStyleBackColor = false;
+            this.selectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // deleteALLButton
+            // 
+            this.deleteALLButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.deleteALLButton.Location = new System.Drawing.Point(17, 286);
+            this.deleteALLButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteALLButton.Name = "deleteALLButton";
+            this.deleteALLButton.Size = new System.Drawing.Size(79, 55);
+            this.deleteALLButton.TabIndex = 5;
+            this.deleteALLButton.Text = "Delete Graph";
+            this.deleteALLButton.UseVisualStyleBackColor = false;
+            this.deleteALLButton.Click += new System.EventHandler(this.DeleteALLButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.deleteButton.Location = new System.Drawing.Point(17, 223);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(79, 55);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // drawEdgeButton
+            // 
+            this.drawEdgeButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.drawEdgeButton.Location = new System.Drawing.Point(16, 160);
+            this.drawEdgeButton.Margin = new System.Windows.Forms.Padding(4);
+            this.drawEdgeButton.Name = "drawEdgeButton";
+            this.drawEdgeButton.Size = new System.Drawing.Size(80, 55);
+            this.drawEdgeButton.TabIndex = 2;
+            this.drawEdgeButton.Text = "E";
+            this.drawEdgeButton.UseVisualStyleBackColor = false;
+            this.drawEdgeButton.Click += new System.EventHandler(this.DrawEdgeButton_Click);
+            // 
+            // drawVertexButton
+            // 
+            this.drawVertexButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.drawVertexButton.Location = new System.Drawing.Point(16, 97);
+            this.drawVertexButton.Margin = new System.Windows.Forms.Padding(4);
+            this.drawVertexButton.Name = "drawVertexButton";
+            this.drawVertexButton.Size = new System.Drawing.Size(80, 55);
+            this.drawVertexButton.TabIndex = 1;
+            this.drawVertexButton.Text = "V";
+            this.drawVertexButton.UseVisualStyleBackColor = false;
+            this.drawVertexButton.Click += new System.EventHandler(this.DrawVertexButton_Click);
+            // 
+            // sheet
+            // 
+            this.sheet.BackColor = System.Drawing.SystemColors.Control;
+            this.sheet.Location = new System.Drawing.Point(104, 34);
+            this.sheet.Margin = new System.Windows.Forms.Padding(4);
+            this.sheet.Name = "sheet";
+            this.sheet.Size = new System.Drawing.Size(845, 511);
+            this.sheet.TabIndex = 0;
+            this.sheet.TabStop = false;
+            this.sheet.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Sheet_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(18, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 31);
+            this.label1.TabIndex = 14;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 31);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "degree";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(1292, 590);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cycleButton);
+            this.Controls.Add(this.chainButton);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.listBoxMatrix);
+            this.Controls.Add(this.deleteALLButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.drawEdgeButton);
+            this.Controls.Add(this.drawVertexButton);
+            this.Controls.Add(this.sheet);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "МОЙ ГРАФ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sheet;
+        private System.Windows.Forms.Button drawVertexButton;
+        private System.Windows.Forms.Button drawEdgeButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button deleteALLButton;
+        private System.Windows.Forms.ListBox listBoxMatrix;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Button chainButton;
+        private System.Windows.Forms.Button cycleButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SystAnalys_lr1/MainCod.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystAnalys_lr1
+{
+    class Vertex
+    {
+        public int x, y;
+
+        public Vertex(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Edge
+    {
+        public int v1, v2;
+
+        public Edge(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+    }
+
+    class DrawGraph
+    {
+        Bitmap Bitmap;
+        Pen BlackPen;
+        Pen RedPen;
+        Pen BlackPen2;
+        Pen Bluepen;
+        Graphics Gr;
+        Font Fo;
+        Brush Br;
+        PointF point;
+        public int R = 25; //радиус окружности вершины
+
+        public DrawGraph(int width, int height)
+        {
+            
+            Bitmap = new Bitmap(width, height);
+            Gr = Graphics.FromImage(Bitmap);
+            ClearSheet();
+            BlackPen = new Pen(Color.Black);
+            BlackPen.Width = 3;
+            RedPen = new Pen(Color.Red);
+            RedPen.Width = 3;
+            BlackPen2 = new Pen(Color.Black);
+            BlackPen2.Width = 2;
+            Fo = new Font("Arial", 15);
+            Br = Brushes.Black;
+            Bluepen = new Pen(Color.Blue);
+            Bluepen.Width = 3;
+        }
+
+        public Bitmap GetBitmap()
+        {
+            return Bitmap;
+        }
+
+        public void ClearSheet()
+        {
+            Gr.Clear(Color.White);
+        }
+
+        public void DrawVertex(int x, int y, string number)
+        {
+            Gr.FillEllipse(Brushes.White, (x - R), (y - R), 2 * R, 2 * R);
+            Gr.DrawEllipse(BlackPen, (x - R), (y - R), 2 * R, 2 * R);
+            point = new PointF(x - 8, y - 10);
+            Gr.DrawString(number, Fo, Br, point);
+        }
+
+        public void DrawSelectedVertex(int x, int y)
+        {
+            Gr.DrawEllipse(RedPen, (x - R), (y - R), 2 * R, 2 * R);
+        }
+
+        public void DrawEdge(Vertex V1, Vertex V2, Edge E, int numberE)
+        {
+            if (E.v1 == E.v2)
+            {
+                Gr.DrawArc(Bluepen, (V1.x - 2 * R), (V1.y - 2 * R), 2 * R, 2 * R, 90, 270);
+                point = new PointF(V1.x - (int)(2.75 * R), V1.y - (int)(2.75 * R));
+                Gr.DrawString(((char)('a' + numberE)).ToString(), Fo, Br, point);
+                DrawVertex(V1.x, V1.y, (E.v1 + 1).ToString());
+            }
+            else
+            {
+                Gr.DrawLine(Bluepen, V1.x, V1.y, V2.x, V2.y);
+                point = new PointF((V1.x + V2.x) / 2, (V1.y + V2.y) / 2);
+                Gr.DrawString(((char)('a' + numberE)).ToString(), Fo, Br, point);
+                DrawVertex(V1.x, V1.y, (E.v1 + 1).ToString());
+                DrawVertex(V2.x, V2.y, (E.v2 + 1).ToString());
+            }
+        }
+
+        public void DrawALLGraph(List<Vertex> V, List<Edge> E)
+        {
+            //рисуем ребра
+            for (int i = 0; i < E.Count; i++)
+            {
+                if (E[i].v1 == E[i].v2)
+                {
+                    Gr.DrawArc(Bluepen, (V[E[i].v1].x - 2 * R), (V[E[i].v1].y - 2 * R), 2 * R, 2 * R, 90, 270);
+                    point = new PointF(V[E[i].v1].x - (int)(2.75 * R), V[E[i].v1].y - (int)(2.75 * R));
+                    Gr.DrawString(((char)('a' + i)).ToString(), Fo, Br, point);
+                    
+                }
+                else
+                {
+                    Gr.DrawLine(Bluepen, V[E[i].v1].x, V[E[i].v1].y, V[E[i].v2].x, V[E[i].v2].y);
+                    point = new PointF((V[E[i].v1].x + V[E[i].v2].x) / 2, (V[E[i].v1].y + V[E[i].v2].y) / 2);
+                    Gr.DrawString(((char)('a' + i)).ToString(), Fo, Br, point);
+                }
+            }
+            //рисуем вершины
+            for (int i = 0; i < V.Count; i++)
+            {
+                DrawVertex(V[i].x, V[i].y, (i + 1).ToString());
+            }
+        }
+
+        //заполняет матрицу смежности
+        public void FillAdjacencyMatrix(int numberV, List<Edge> E, int[,] matrix)
+        {
+            for (int i = 0; i < numberV; i++)
+                for (int j = 0; j < numberV; j++)
+                    matrix[i, j] = 0;
+            for (int i = 0; i < E.Count; i++)
+            {
+                matrix[E[i].v1, E[i].v2] = 1;
+                matrix[E[i].v2, E[i].v1] = 1;
+            }
+        }
+
+        //заполняет матрицу инцидентности
+        public void FillIncidenceMatrix(int numberV, List<Edge> E, int[,] matrix)
+        {
+            for (int i = 0; i < numberV; i++)
+                for (int j = 0; j < E.Count; j++)
+                    matrix[i, j] = 0;
+            for (int i = 0; i < E.Count; i++)
+            {
+                matrix[E[i].v1, i] = 1;
+                matrix[E[i].v2, i] = 1;
+            }
+        }
+
+        
+    }
+}
